c7899235cb631ea1b876ea8258ca3918e4f28983
ctznfive <ctznfive@gmail.com>
2022-02-02 14:17:17 +0300

    create a function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+function getRandom() {
+    return Math.floor(Math.random() * 3);
+}
+
+function computerPlay() {
+    if (getRandom() === 0) return "Rock";
+    else if (getRandom() === 1) return "Paper";
+    else return "Scissors";
+}
