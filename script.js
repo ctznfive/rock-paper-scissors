47a387aa6f945f8de1015a44ea9120ac3beb0e75
ctznfive <ctznfive@gmail.com>
2022-02-02 15:24:40 +0300

    Create a function that plays a single round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function computerPlay() {
     else if (getRandom() === 1) return "Paper";
     else return "Scissors";
 }
+
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+
+    if (player === "rock") {
+        if (computer === "scissors") return "You Win! Rock beats Scissors";
+        else if (computer === "paper") return "You Lose! Paper beats Rock";
+        else if (computer === "rock") return "It's a Draw";
+        else return "Something Went Wrong";
+    } else if (player === "paper") {
+        if (computer === "rock") return "You Win! Paper beats Rock";
+        else if (computer === "scissors") return "You Lose! Scissors beats Paper";
+        else if (computer === "paper") return "It's a Draw";
+        else return "Something Went Wrong";
+    } else if (player === "scissors") {
+        if (computer === "paper") return "You Win! Scissors beats Paper";
+        else if (computer === "rock") return "You Lose! Rock beats Scissors";
+        else if (computer === "scissors") return "It's a Draw";
+        else return "Something Went Wrong";
+    } else {
+        return "That's not a valid choice";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
